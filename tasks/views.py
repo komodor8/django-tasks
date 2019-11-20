c560ca5e5ca2fe8f716394bd7d89761b355a50d0
farid <flkhaldouni@safehost.com>
2019-11-20 16:31:07 +0100

    add confirm delete resource

--- tasks/views.py
+++ tasks/views.py
@@ -25,7 +25,6 @@ class DetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tasks'] = Task.objects.all()
-        print(context['tasks'])
         return context
 
 
@@ -39,6 +38,6 @@ class UpdateView(UpdateView):
     model = Task
 
 
-class TaskDeleteView(DeleteView):
+class DeleteView(DeleteView):
     model = Task
     success_url = reverse_lazy('tasks:index')
