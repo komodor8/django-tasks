29728cd80da592d4d1545bb89c84a23f8d10313f
farid <flkhaldouni@safehost.com>
2019-11-26 16:35:26 +0100

    update comments + add front buttons

--- tasks/urls.py
+++ tasks/urls.py
@@ -10,5 +10,19 @@ urlpatterns = [
     path('<int:pk>/', views.TaskDetailView.as_view(), name='detail'),
     path('update/<int:pk>', views.UpdateView.as_view(), name='update'),
     path('delete/<int:pk>', views.DeleteView.as_view(), name='delete'),
-    path('<int:task>/create/comment', views.CreateCommentView.as_view(), name='create-comment'),
+    path(
+        '<int:task>/comments/create',
+        views.CreateCommentView.as_view(),
+        name='comments-create'
+    ),
+    path(
+        '<int:task>/comments/<int:pk>/delete/',
+        views.DeleteCommentView.as_view(),
+        name='comments-delete'
+    ),
+    path(
+        '<int:task>/comments/<int:pk>/update/',
+        views.UpdateCommentView.as_view(),
+        name='comments-update'
+    ),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,8 @@
-from django.shortcuts import redirect, render
 from django.views.generic import CreateView, TemplateView, UpdateView, FormView
 from .models import Task, TaskForm, Comment, CommentForm
 from django.views.generic.detail import DetailView
-from django.urls import reverse_lazy, reverse
+from django.urls import reverse_lazy
 from django.views.generic.edit import DeleteView
-from django.views.generic.detail import SingleObjectMixin
-from django.views import View
 from django.views.generic.edit import ModelFormMixin
 
 # Create your views here.
@@ -16,7 +13,7 @@ class IndexView(TemplateView):
     model = Task
 
     def get_context_data(self, **kwargs):
-        context = super(IndexView, self).get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         context['tasks'] = Task.objects.all()
         return context
 
@@ -33,6 +30,25 @@ class TaskDetailView(DetailView):
         return context
 
 
+class CreateView(CreateView):
+    form_class = TaskForm
+    model = Task
+
+
+class UpdateView(UpdateView):
+    form_class = TaskForm
+    model = Task
+
+
+class DeleteView(DeleteView):
+    model = Task
+    success_url = reverse_lazy('tasks:index')
+
+
+#################################################
+#################################################
+
+
 class CreateCommentView(ModelFormMixin, FormView):
     template_name = 'tasks/detail.html'
     form_class = CommentForm
@@ -47,16 +63,18 @@ class CreateCommentView(ModelFormMixin, FormView):
         return str(reverse_lazy('tasks:detail', kwargs={'pk': task_id}))
 
 
-class CreateView(CreateView):
-    form_class = TaskForm
-    model = Task
+class DeleteCommentView(DeleteView):
+    model = Comment
+
+    def get_success_url(self):
+        task_id = self.kwargs['task']
+        return str(reverse_lazy('tasks:detail', kwargs={'pk': task_id}))
 
 
-class UpdateView(UpdateView):
-    form_class = TaskForm
-    model = Task
+class UpdateCommentView(UpdateView):
+    model = Comment
+    form_class = CommentForm
 
-
-class DeleteView(DeleteView):
-    model = Task
-    success_url = reverse_lazy('tasks:index')
+    def get_success_url(self):
+        task_id = self.kwargs['task']
+        return str(reverse_lazy('tasks:detail', kwargs={'pk': task_id}))
