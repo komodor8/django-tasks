353e6685139fa2e4a1046c3bc65f1f449e0e4480
farid <flkhaldouni@safehost.com>
2019-11-27 16:16:11 +0100

    set templates and urlconf for accounts app

--- mysite/urls.py
+++ mysite/urls.py
@@ -26,6 +26,8 @@ urlpatterns = [
     path('polls/', include('polls.urls')),
     path('tasks/', include('tasks.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 
